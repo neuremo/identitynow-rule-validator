fc753757a1430760c5083abe5fc6992d6aa6e663
Neil McGlennon <neil.mcglennon@sailpoint.com>
2020-01-28 15:26:20 -0600

    Create validate.rb

---
+++ examples/validate.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+require 'identitynow_rule_validator'
+
+input = "/Users/neil.mcglennon/Documents/Workspace/identitynow-services-config/"
+
+IdentityNowRuleValidator.validate_directory( input )
